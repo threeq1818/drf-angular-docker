cdb9a143ca50a5a12f7f450cda05b877e710106d
Three Q <threeq1818@gmail.com>
2019-09-19 20:06:45 +0800

    added todoapi/tasklist views

--- django/todoapi/views.py
+++ django/todoapi/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Task
+from .serializers import TaskSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class TaskList(APIView):
+    """
+    For GET requests.
+    View all tasks.
+    """
+    def get(self, request, format=None):
+        """
+        :param request:
+        :param format:
+        :return: a list of all tasks.
+        """
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
