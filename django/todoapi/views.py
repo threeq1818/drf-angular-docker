86c58a7e3b845a7c86d4b9b79a9cf009be72f301
Three Q <threeq1818@gmail.com>
2019-09-20 01:16:07 +0800

    Implemented using RetrieveUpdateDestroyAPIView for detail, update, delete

--- django/todoapi/views.py
+++ django/todoapi/views.py
@@ -7,6 +7,7 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 # Create your views here.
 
 class TaskList(generics.ListCreateAPIView):
@@ -16,30 +17,10 @@ class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-class TaskDetail(APIView):
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Returns a single Task and allows updates and deletion of a Task.
+    Return a single Task and allows updates and deletion of a Task.
     """
-    def get_object(self, task_id):
-        try:
-            return Task.objects.get(pk=task_id)
-        except Task.DoesNotExist:
-            raise Http404
-
-    def get(self, request, task_id, format=None):
-        task = self.get_object(task_id)
-        serializer = TaskSerializer(task)
-        return Response(serializer.data)
-
-    def put(self, request, task_id, format=None):
-        task = self.get_object(task_id)
-        serializer = TaskSerializer(task, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, task_id, format=None):
-        task = self.get_object(task_id)
-        task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_url_kwarg = 'task_id'
