ac80d2851c89578f133c39c71a6fb19a47df70f4
Three Q <threeq1818@gmail.com>
2019-09-23 03:24:56 +0800

    added an Angular Service to Call the API

--- angular/angular-app/src/app/app.module.ts
+++ angular/angular-app/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { TaskListComponent } from './task-list/task-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
